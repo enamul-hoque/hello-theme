5b664cf8bff75f07373197e95b80ebd8aecc8865
Enamul Hoque Mohon <enamul.hoque.mohon@gmail.com>
2022-04-20 10:02:07 +0600

    update: Add To Cart text option.

--- inc/woo-options.php
+++ inc/woo-options.php
@@ -3,31 +3,37 @@
  * Table of Contents
  * -----------------
  * 
- * 'Add To Cart' Text Option for Shop and Archive Pages.
- * 'Add To Cart' Text Option for Product Single Page.
- * Customizer Option: 'Add To Cart' Texts.
+ * Add To Cart Texts Option.
+ * Customizer Option: Add To Cart Texts.
  */
 
 
-/** 'Add To Cart' Text Option for Shop and Archive Pages. */
-function webex_shop_cart_btn_text() {
-    return get_theme_mod( 'webex_shop_cart_btn_text', __( 'Add To Cart', 'woocommerce' ) );
-}
-add_filter( 'woocommerce_product_add_to_cart_text', 'webex_shop_cart_btn_text' );
+/** Add To Cart Texts Option. */
+if ( get_theme_mod( 'webex_cart_btn_text' ) ):
+    function webex_shop_cart_btn_text() {
+        return get_theme_mod( 'webex_shop_cart_btn_text', __( 'Add To Cart', 'woocommerce' ) );
+    }
+    add_filter( 'woocommerce_product_add_to_cart_text', 'webex_shop_cart_btn_text' );
+
+    function webex_single_cart_btn_text() {
+        return get_theme_mod( 'webex_single_cart_btn_text', __( 'Add To Cart', 'woocommerce' ) );
+    }
+    add_filter( 'woocommerce_product_single_add_to_cart_text', 'webex_single_cart_btn_text' );
+endif;
 
 
-/** 'Add To Cart' Text Option for Product Single Page. */
-function webex_single_cart_btn_text() {
-    return get_theme_mod( 'webex_single_cart_btn_text', __( 'Add To Cart', 'woocommerce' ) );
-}
-add_filter( 'woocommerce_product_single_add_to_cart_text', 'webex_single_cart_btn_text' );
-
-
-/** Customizer Option: 'Add To Cart' Texts. */
+/** Customizer Option: Add To Cart Texts. */
 function webex_cart_btn_texts( $wp_customize ) {
+    $wp_customize->add_setting( 'webex_cart_btn_text', []);
+        $wp_customize->add_control( 'webex_cart_btn_text', [
+            'label'    => __( 'Enable Custom Add To Cart Texts below.', 'webex' ),
+            'section'  => 'woocommerce_product_catalog',
+            'settings' => 'webex_cart_btn_text',
+            'type'     => 'checkbox',
+        ]);
+
     $wp_customize->add_setting( 'webex_shop_cart_btn_text', [
         'default'   => __( 'Add To Cart', 'woocommerce' ),
-        'transport' => 'refresh',
     ]);
         $wp_customize->add_control( 'webex_shop_cart_btn_text', [
             'label'     => __( 'Add To Cart Text: Shop/Archive Pages', 'woocommerce' ),
@@ -38,7 +44,6 @@ function webex_cart_btn_texts( $wp_customize ) {
     
     $wp_customize->add_setting( 'webex_single_cart_btn_text', [
         'default'   => __( 'Add To Cart', 'woocommerce' ),
-        'transport' => 'refresh',
     ]);
         $wp_customize->add_control( 'webex_single_cart_btn_text', [
             'label'     => __( 'Add To Cart Text: Single Pages', 'woocommerce' ),
